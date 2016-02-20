d067e40dac02c8071f0f4d9f0dc74e118405f46c
ayeo <me@ayeo.pl>
2016-02-20 14:53:40 +0100

    Initial

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,28 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+    Texture img2;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img2 = new Texture("406.png");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img2, 220, 220, 250 , 170);
+		batch.end();
+	}
+}
