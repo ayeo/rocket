8dac5ac631309af98921a3c840b90d06e4709b8a
ayeo <me@ayeo.pl>
2016-02-21 16:32:17 +0100

    Adapt to be rocket game

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -9,141 +9,80 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
 
-import java.util.Iterator;
 
-public class MyGdxGame extends ApplicationAdapter {
-//	SpriteBatch batch;
-//	Texture img;
-//    Texture img2;
+public class MyGdxGame extends ApplicationAdapter
+{
+    private Texture rocketImage;
 
-    private Texture dropImage;
-    private Texture bucketImage;
     private Sound dropSound;
-    private Music rainMusic;
 
     private OrthographicCamera camera;
     private SpriteBatch batch;
 
     private Rectangle bucket;
 
-    private Array<Rectangle> raindrops;
-    private long lastDropTime;
+    private Rocket rocket;
 
 	@Override
-	public void create () {
-//		batch = new SpriteBatch();
-//		img2 = new Texture("406.png");
-		// load the images for the droplet and the bucket, 64x64 pixels each
-		dropImage = new Texture(Gdx.files.internal("core/assets/droplet.png"));
-		bucketImage = new Texture(Gdx.files.internal("core/assets/bucket.png"));
-
-		// load the drop sound effect and the rain background "music"
+	public void create()
+    {
+        rocketImage = new Texture(Gdx.files.internal("core/assets/rocket.png"));
 		dropSound = Gdx.audio.newSound(Gdx.files.internal("core/assets/drop.wav"));
-		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("core/assets/rain.mp3"));
-
-		// start the playback of the background music immediately
-		rainMusic.setLooping(true);
-		rainMusic.play();
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
 
         batch = new SpriteBatch();
-
-        bucket = new Rectangle();
-        bucket.x = 800 / 2 - 64 / 2;
-        bucket.y = 20;
-        bucket.width = 64;
-        bucket.height = 64;
-
-        // create the raindrops array and spawn the first raindrop
-        raindrops = new Array<Rectangle>();
-        spawnRaindrop();
+        rocket = new Rocket(100, 100);
 	}
+
+    private void updateRocketPosition()
+    {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+        {
+            rocket.moveLeft(200 * Gdx.graphics.getDeltaTime());
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+        {
+            rocket.moveRight(200 * Gdx.graphics.getDeltaTime());
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.UP))
+        {
+            rocket.moveUp(200 * Gdx.graphics.getDeltaTime());
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
+        {
+            rocket.moveDown(200 * Gdx.graphics.getDeltaTime());
+        }
+    }
 
 	@Override
 	public void render () {
-//		Gdx.gl.glClearColor(1, 1, 1, 1);
-//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//		batch.begin();
-//		batch.draw(img2, 220, 220, 250 , 170);
-//		batch.end();
 
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
 
-        batch.setProjectionMatrix(camera.combined);
-
-        // begin a new batch and draw the bucket and
-        // all drops
-        batch.begin();
-        batch.draw(bucketImage, bucket.x, bucket.y);
-        for(Rectangle raindrop: raindrops) {
-            batch.draw(dropImage, raindrop.x, raindrop.y);
-        }
-        batch.end();
+        batch.setProjectionMatrix(camera.combined); //todo: check what this does
 
         batch.begin();
-        batch.draw(bucketImage, bucket.x, bucket.y);
+        batch.draw(rocketImage, rocket.getX(), rocket.getY(), 46, 80);
         batch.end();
 
-        if(Gdx.input.isTouched()) {
-            Vector3 touchPos = new Vector3();
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            camera.unproject(touchPos);
-            bucket.x = touchPos.x - 64 / 2;
-        }
-
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
-
-        if(bucket.x < 0) bucket.x = 0;
-        if(bucket.x > 800 - 64) bucket.x = 800 - 64;
-
-        // check if we need to create a new raindrop
-        if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
-
-        // move the raindrops, remove any that are beneath the bottom edge of
-        // the screen or that hit the bucket. In the later case we play back
-        // a sound effect as well.
-        Iterator<Rectangle> iter = raindrops.iterator();
-        while(iter.hasNext()) {
-            Rectangle raindrop = iter.next();
-            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-            if(raindrop.y + 64 < 0) iter.remove();
-
-            if(raindrop.overlaps(bucket)) {
-                dropSound.play();
-                iter.remove();
-            }
-        }
+        updateRocketPosition();
 	}
 
-    private void spawnRaindrop() {
-        Rectangle raindrop = new Rectangle();
-        raindrop.x = MathUtils.random(0, 800-64);
-        raindrop.y = 480;
-        raindrop.width = 64;
-        raindrop.height = 64;
-        raindrops.add(raindrop);
-        lastDropTime = TimeUtils.nanoTime();
-    }
 
     @Override
-    public void dispose() {
-        // dispose of all the native resources
-        dropImage.dispose();
-        bucketImage.dispose();
+    public void dispose()
+    {
         dropSound.dispose();
-        rainMusic.dispose();
         batch.dispose();
     }
 
---
+++ core/src/com/mygdx/game/Rocket.java
@@ -0,0 +1,48 @@
+package com.mygdx.game;
+
+public class Rocket
+{
+    private int x;
+
+    private int y;
+
+    private double velocity;
+
+    private double angle;
+
+    public Rocket(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return this.x;
+    }
+
+    public int getY()
+    {
+        return this.y;
+    }
+
+    public void moveLeft(float value)
+    {
+        this.x -= value;
+    }
+
+    public void moveRight(float value)
+    {
+        this.x += value;
+    }
+
+    public void moveDown(float value)
+    {
+        this.y -= value;
+    }
+
+    public void moveUp(float value)
+    {
+        this.y += value;
+    }
+}
